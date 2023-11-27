0fc50d4389042b78190e241d30c4b6350a3c3e07
Martin Kondor <martinkondor@gmail.com>
2023-11-27 21:46:37 +0100

    Initial commit

---
+++ src/test/java/com/martinkondor/textshare/TextShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinkondor.textshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
